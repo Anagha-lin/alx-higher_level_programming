ef391e2cf891d9f45d12e46619a30291f9f38be0
Anagha-lin <linusanaghamay12.com>
2023-11-04 01:21:45 -0700

    s

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,15 +1,4 @@
 #!/usr/bin/python3
-
-from ctypes import c_int
-
-class alx_listint_t:
-    pass
-
-alx_listint_t.n = c_int()
-alx_listint_t.next = None
-
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        if isinstance(num, int):
-            alx_listint_t.n = num
-            print("{:d}".format(alx_listint_t.n))
+    for i in range(len(my_list)):
+        print("{:d}".format(my_list[i]))
