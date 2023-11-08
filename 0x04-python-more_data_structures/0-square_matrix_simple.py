64b9b3ae710d73351a125b00e6ca07fde4421304
Anagha-lin <linusanaghamay12.com>
2023-11-08 10:25:57 -0800

    a

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-# -----------------------------------------------------------
-# Python program :
-# demonstrates how to compute the square value of all integers of a matrix
-#
-# (C) 2023 Anagha Linus
-# -----------------------------------------------------------
-
-
 def square_matrix_simple(matrix=[]):
-    new_matrix = [[number**2 for number in row] for row in matrix]
+    new_matrix = []
+    for row in matrix:
+        new_matrix.append([number ** 2 for number in row])
     return new_matrix
