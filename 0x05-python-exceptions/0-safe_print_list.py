d4e543066ff02626280d1f7cedb61b364fed490d
Anagha-lin <linusanaghamay12.com>
2023-11-20 11:30:58 -0800

    a

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -19,11 +19,3 @@ def safe_print_list(my_list=[], x=0):
 
     return count
 
-# Example usage:
-if __name__ == "__main__":
-    my_list = [1, 2, 3, "four", 5]
-
-    # Print 3 elements from the list
-    elements_printed = safe_print_list(my_list, 3)
-    print("Number of elements printed:", elements_printed)
-
