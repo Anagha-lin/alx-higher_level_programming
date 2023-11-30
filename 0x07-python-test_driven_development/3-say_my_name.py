650bdb07c608e99e2a2d2d690735cd2674d85d0b
Anagha-lin <linusanaghamay12.com>
2023-11-30 13:56:04 -0800

    a

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,8 +1,29 @@
+#!/usr/bin/python3
+"""
+the name project.
+"""
+
 def say_my_name(first_name, last_name=""):
+    """
+    Print a message introducing the provided names.
+
+    Args:
+        first_name (str): String representing the first name.
+        last_name (str): String representing the last name (optional).
+
+    Raises:
+        TypeError: If either first_name or last_name is not a string.
+    """
     # Check if the provided names are strings
     if not all(isinstance(name, str) for name in (first_name, last_name)):
         raise TypeError('Both first_name and last_name must be strings')
 
     # Print a message introducing the names
-    print(f"My name is {first_name} {last_name}".strip())
+    full_name = f"My name is {first_name} {last_name}"
+    # Print the full name without an extra space at the end
+    print(full_name if last_name else first_name)
+
+# Example usage:
+# say_my_name("John", "Doe")
+# say_my_name("Alice")
 
