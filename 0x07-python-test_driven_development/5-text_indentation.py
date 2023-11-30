c39507f4c2400fb4d6698853dbe87f3f1eab745c
Anagha-lin <linusanaghamay12.com>
2023-11-30 14:16:18 -0800

    a

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,33 @@
 def text_indentation(text):
-    # Check if text is a string
+    """
+    Print text with 2 new lines after each '.', '?', and ':' characters.
+
+    Args:
+        text (str): The input text.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
     if not isinstance(text, str):
-        raise TypeError("text must be a string")
+        raise TypeError('text must be a string')
 
-    # Split the text based on '.', '?', and ':'
-    separators = ['.', '?', ':']
-    lines = []
-    current_line = ""
+    # Split the text into sentences using '.', '?', and ':'
+    sentences = [sentence.strip() for sentence in text.split('.') + text.split('?') + text.split(':')]
 
-    for char in text:
-        current_line += char
+    # Filter out empty sentences
+    sentences = [sentence for sentence in sentences if sentence]
 
-        if char in separators:
-            lines.append(current_line.strip())
-            current_line = ""
+    # Print each sentence with 2 new lines
+    print("\n\n".join(sentences))
 
-    # Add the last line if it's not empty
-    if current_line:
-        lines.append(current_line.strip())
+# Example usage:
+# text_indentation("""Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+# Quonam modo? Utrum igitur tibi litteram videor an totas paginas commovere? \
+# Non autem hoc: igitur ne illud quidem. Fortasse id optimum, sed ubi illud: \
+# Plus semper voluptatis? Teneo, inquit, finem illi videri nihil dolere. \
+# Transfer idem ad modestiam vel temperantiam, quae est moderatio cupiditatum \
+# rationi oboediens. Si id dicis, vicimus. Inde sermone vario sex illa a Dipylo \
+# stadia confecimus. Sin aliud quid voles, postea. Quae animi affectio suum \
+# cuique tribuens atque hanc, quam dico. Utinam quidem dicerent alium alio \
+# beatiorem! Iam ruinas videres""")
 
-    # Print the lines with 2 new lines after '.', '?', and ':'
-    for line in lines:
-        print(line)
-        print()
