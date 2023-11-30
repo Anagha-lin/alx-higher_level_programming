c0b6d59bfe4b5003d378819d49fc276ee25d84a0
Anagha-lin <linusanaghamay12.com>
2023-11-30 13:45:02 -0800

    a

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,12 +1,8 @@
 def say_my_name(first_name, last_name=""):
-    # Check if the provided first name is a string
-    if not isinstance(first_name, str):
-        raise TypeError('first_name must be a string')
+    # Check if the provided names are strings
+    if not all(isinstance(name, str) for name in (first_name, last_name)):
+        raise TypeError('Both first_name and last_name must be strings')
 
-    # Check if the provided last name is a string
-    if not isinstance(last_name, str):
-        raise TypeError('last_name must be a string')
-
-    # Print a message introducing the provided names
+    # Print a message introducing the names
     print(f"My name is {first_name} {last_name}".strip())
 
