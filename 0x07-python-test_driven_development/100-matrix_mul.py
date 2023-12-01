e605c92f1340f2da3d79023b8d56f3d6e8f13b8c
Anagha-lin <linusanaghamay12.com>
2023-12-01 15:13:22 -0800

    a

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,49 +1,128 @@
-#!/usr/bin/python3
-"""
-Matrix multiplication module.
-"""
+class ShashNode:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.next = None
+        self.snext = None
+        self.sprev = None
 
 
-def matrix_mul(m_a, m_b):
-    """
-    Multiplies two matrices.
+class ShashTable:
+    def __init__(self, size):
+        self.size = size
+        self.shead = None
+        self.stail = None
+        self.array = [None] * size
 
-    Args:
-        m_a (list of lists): First matrix.
-        m_b (list of lists): Second matrix.
+    def add_to_sorted_list(self, node):
+        temp_var = self.shead
 
-    Returns:
-        list of lists: Resulting matrix.
+        if not temp_var and not self.stail:
+            self.shead = self.stail = node
+            return
 
-    Raises:
-        TypeError: If m_a or m_b is not a list, not a list of lists, or contains non-numeric elements.
-        ValueError: If m_a or m_b is empty, not a rectangle, or cannot be multiplied.
-    """
-    # Matrix validation function
-    def validate_matrix(matrix):
-        if not isinstance(matrix, list) or not all(isinstance(row, list) for row in matrix) or not matrix or any(not row for row in matrix):
-            raise ValueError(f"{matrix} can't be empty or is not a list of lists")
-        if any(not isinstance(element, (int, float)) for row in matrix for element in row):
-            raise TypeError(f"{matrix} should contain only integers or floats")
-        if any(len(row) != len(matrix[0]) for row in matrix):
-            raise TypeError(f"Each row of {matrix} must be of the same size")
+        while temp_var:
+            if node.key < temp_var.key:
+                node.snext, node.sprev = temp_var, temp_var.sprev
+                temp_var.sprev = node
+                if node.sprev:
+                    node.sprev.snext = node
+                else:
+                    self.shead = node
+                return
+            temp_var = temp_var.snext
 
-    # Validate matrices
-    validate_matrix(m_a)
-    validate_matrix(m_b)
+        node.sprev = self.stail
+        self.stail.snext = node
+        self.stail = node
 
-    # Check if matrices can be multiplied
-    if len(m_a[0]) != len(m_b):
-        raise ValueError("m_a and m_b can't be multiplied")
+    def shash_table_set(self, key, value):
+        if not self.array or self.size == 0 or not key or not value:
+            return False
 
-    # Perform matrix multiplication using list comprehension
-    result_matrix = [[sum(a * b for a, b in zip(row_a, col_b)) for col_b in zip(*m_b)] for row_a in m_a]
+        index = hash(key) % self.size
+        temp_var = self.array[index]
 
-    return result_matrix
+        while temp_var:
+            if temp_var.key == key:
+                temp_var.value = value
+                return True
+            temp_var = temp_var.next
+
+        sh_node = ShashNode(key, value)
+        sh_node.next, self.array[index] = self.array[index], sh_node
+        self.add_to_sorted_list(sh_node)
+        return True
+
+    def shash_table_get(self, key):
+        if not self.array or self.size == 0 or not key:
+            return None
+
+        index = hash(key) % self.size
+        temp_var = self.array[index]
+
+        while temp_var:
+            if temp_var.key == key:
+                return temp_var.value
+            temp_var = temp_var.next
+
+        return None
+
+    def shash_table_print(self):
+        if not self.array:
+            return
+
+        result = "{"
+        temp_var = self.shead
+
+        while temp_var:
+            result += f"'{temp_var.key}': '{temp_var.value}'"
+            temp_var = temp_var.snext
+            if temp_var:
+                result += ", "
+
+        result += "}"
+        print(result)
+
+    def shash_table_print_rev(self):
+        if not self.array:
+            return
+
+        result = "{"
+        temp_var = self.stail
+
+        while temp_var:
+            result += f"'{temp_var.key}': '{temp_var.value}'"
+            temp_var = temp_var.sprev
+            if temp_var:
+                result += ", "
+
+        result += "}"
+        print(result)
+
+    def shash_table_delete(self):
+        if not self.array or self.size == 0:
+            return
+
+        for i in range(self.size):
+            while self.array[i]:
+                next_node, self.array[i] = self.array[i].next, None
+                del next_node
+
+        self.array = None
+        self.shead = self.stail = None
+        self.size = 0
 
 
 # Example usage:
-# Uncomment the lines below to test the function with the provided matrices
-# print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-# print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+# Uncomment the lines below to test the ShashTable class
+
+# shash_table = ShashTable(10)
+# shash_table.shash_table_set("key1", "value1")
+# shash_table.shash_table_set("key2", "value2")
+# shash_table.shash_table_print()
+# shash_table.shash_table_print_rev()
+# print(shash_table.shash_table_get("key1"))
+# print(shash_table.shash_table_get("key3"))
+# shash_table.shash_table_delete()
 
