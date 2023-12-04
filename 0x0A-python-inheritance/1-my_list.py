eed4a87cdd7f29f033d5e315dac21c8605b8dc42
Anagha-lin <linusanaghamay12.com>
2023-12-04 12:44:51 -0800

    a

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
 
 class MyList(list):
-    """Inherits from list and adds a method to print the sorted list."""
+    """a subclass of list"""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
-        """Prints the list in ascending order."""
+        """prints the sorted list"""
         print(sorted(self))
-
