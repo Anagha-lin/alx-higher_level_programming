681e585d1752b7b52f452523ad6878311faf94bf
Anagha-lin <linusanagha37@gmail.com>
2024-02-16 06:25:22 -0800

    a

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,29 +1,34 @@
 #!/usr/bin/python3
-import MySQLdb
+
+'''
+Lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa.
+'''
+
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Check if correct number of arguments are provided
-    if len(sys.argv) != 4:
-        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
-        sys.exit(1)
-
-    # Retrieve command line arguments
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+    # Get MySQL credentials from command-line arguments
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
     # Connect to MySQL server
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database)
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=mysql_username,
+        passwd=mysql_password,
+        db=database_name
+    )
 
     # Create a cursor object
     cursor = db.cursor()
 
-    # Execute the SQL query to select states starting with 'N'
+    # Execute SQL query to select states starting with 'N'
     cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    # Fetch all rows and print them
+    # Fetch and print the results
     rows = cursor.fetchall()
     for row in rows:
         print(row)
