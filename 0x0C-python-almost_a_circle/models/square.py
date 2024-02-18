92c2962b2881f56ac881a543c374871ee71642b7
Anagha-lin <linusanagha37@yahoo.com>
2024-02-18 13:09:48 -0800

    a

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -33,12 +33,12 @@ class Square(Rectangle):
         self.height = value
 
     def update(self, *args, **kwargs):
-        """Assign arguments to attributes."""
+        """Assign attributes to the square."""
         attrs = ["id", "size", "x", "y"]
         if args:
             for i, arg in enumerate(args):
                 setattr(self, attrs[i], arg)
-        else:
+        elif kwargs:
             for key, value in kwargs.items():
                 setattr(self, key, value)
 
