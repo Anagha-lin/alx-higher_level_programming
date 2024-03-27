09b4ca3b46e4aa9ef7a42e902c2537c588aec00d
Anagha-lin <linusanagha37@gmail.com>
2024-03-27 12:18:18 -0700

    a

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,19 +1,4 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 const request = require('request');
-
-// Extract command-line arguments
-const url = process.argv[2];
-const filePath = process.argv[3];
-
-// Make a GET request and pipe response to file
-request(url)
-  .on('error', (error) => {
-    console.error(error);
-  })
-  .pipe(fs.createWriteStream(filePath, { encoding: 'utf-8' }))
-  .on('finish', () => {
-    console.log(`Successfully saved the contents of ${url} to ${filePath}`);
-  });
-
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
