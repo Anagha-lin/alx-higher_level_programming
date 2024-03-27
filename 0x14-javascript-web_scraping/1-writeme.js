8e9903b33cf05c0d123552bcc849e9800553f637
Anagha-lin <linusanagha37@gmail.com>
2024-03-27 11:56:10 -0700

    a

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 const filePath = process.argv[2];
 const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-    return;
+// Write content to file
+fs.writeFile(filePath, content, 'utf-8', (error) => {
+  if (error) {
+    // Print error object if an error occurs
+    console.error(error);
   }
-  console.log('The file has been saved!');
 });
 
